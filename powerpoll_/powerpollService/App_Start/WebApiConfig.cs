5b8f39a85ee5dcfb1547a995e47d63b759a298c0
Patrick Healy <healyp3@tcd.ie>
2015-02-10 15:06:19 +0000

    add powerpoll service

---
+++ powerpoll_/powerpollService/App_Start/WebApiConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+using powerpollService.DataObjects;
+using powerpollService.Models;
+
+namespace powerpollService
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            // Use this class to set configuration options for your mobile service
+            ConfigOptions options = new ConfigOptions();
+
+            // Use this class to set WebAPI configuration options
+            HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
+
+            // To display errors in the browser during development, uncomment the following
+            // line. Comment it out again when you deploy your service for production use.
+            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            
+            Database.SetInitializer(new powerpollInitializer());
+        }
+    }
+
+    public class powerpollInitializer : ClearDatabaseSchemaIfModelChanges<powerpollContext>
+    {
+        protected override void Seed(powerpollContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
