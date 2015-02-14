9b87caead44c66b3778de0c158a80687152e2bf2
Patrick Healy <healyp3@tcd.ie>
2015-02-14 13:44:51 +0000

    Added an empty TwitterController api

---
+++ powerpoll_/powerpollService/Controllers/TwitterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace powerpollService.Controllers
+{
+    public class TwitterController : ApiController
+    {
+        public ApiServices Services { get; set; }
+
+        // GET api/Twitter
+        public string Get()
+        {
+            Services.Log.Info("Hello from custom controller!");
+            return "Hello";
+        }
+
+    }
+}
