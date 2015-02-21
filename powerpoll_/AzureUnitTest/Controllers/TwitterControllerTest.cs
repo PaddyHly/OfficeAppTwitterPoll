bcbd8fff148e7e7cca5fab72de10b89565c09a97
Pavel <tarasovp@tcd.ie>
2015-02-21 15:57:32 +0000

    Added a template for unit testing within the VS (may be more relevant for back-end)

---
+++ powerpoll_/AzureUnitTest/Controllers/TwitterControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Web;
+using System.Threading.Tasks;
+using powerpollService.Controllers.TwitterController;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tweetinvi;
+using Tweetinvi.Core.Interfaces.Models;
+using powerpollService.DataObjects;
+using Newtonsoft.Json;
+
+namespace powerpollService.Tests
+{
+    [TestClass]
+    public class TwitterControllerTest
+    {
+        [TestMethod]
+        public void GetHashTagAndKeyword();
+        {
+            TwitterController controller = new TwitterController();
+            string hTagKWord = controller.Get("bestanimal", "dog");
+        }
+
+    }
+}
