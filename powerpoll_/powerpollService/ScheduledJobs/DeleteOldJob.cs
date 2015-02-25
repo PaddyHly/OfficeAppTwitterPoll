055337baf2a7e6014dab32d64b2f454f15a6423b
Patrick Healy <healyp3@tcd.ie>
2015-02-25 19:22:26 +0000

    added DeleteOldJob.cs, can be scheduled to ensure old polls are removed from the database, freeing hashtags for reuse

---
+++ powerpoll_/powerpollService/ScheduledJobs/DeleteOldJob.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Mobile.Service;
+using Microsoft.WindowsAzure.Mobile.Service.ScheduledJobs;
+using powerpollService.Models;
+using powerpollService.DataObjects;
+
+
+namespace powerpollService
+{
+    // A simple scheduled job which can be invoked manually by submitting an HTTP
+    // POST request to the path "/jobs/sample".
+
+    public class DeleteOldJob : ScheduledJob
+    {
+        private powerpollContext context;
+
+        protected override void Initialize(ScheduledJobDescriptor scheduledJobDescriptor, CancellationToken cancellationToken)
+        {
+            base.Initialize(scheduledJobDescriptor, cancellationToken);
+
+            context = new powerpollContext();
+        }
+
+        public async override Task ExecuteAsync()
+        {
+            foreach(Poll poll in context.Polls){
+                if (poll.End_Time <= DateTime.Now)
+                {
+                    context.Polls.Remove(poll);
+                }
+            }
+            await context.SaveChangesAsync();
+        }
+    }
+}
