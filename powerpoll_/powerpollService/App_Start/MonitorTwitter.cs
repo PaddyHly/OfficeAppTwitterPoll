4692e04125140f4ebbc3d99367fc4697fd92039f
Patrick Healy <healyp3@tcd.ie>
2015-03-11 19:15:35 +0000

    fixed two bugs in the nack end, fixed some things that caused warnings in the front end

--- powerpoll_/powerpollService/App_Start/MonitorTwitter.cs
+++ powerpoll_/powerpollService/App_Start/MonitorTwitter.cs
@@ -18,17 +18,17 @@ namespace powerpollService
     {
         public static void monitor()
         {
-            powerpollContext context = new powerpollContext();
             var stream = Stream.CreateUserStream();
             stream.TweetCreatedByAnyone += (s, t) =>
             {
+                powerpollContext context = new powerpollContext();
                 var hashtags = t.Tweet.Hashtags.ToArray()
                     .Select(x => x.Text.ToLowerInvariant());
                 foreach (Poll poll in context.Polls.ToArray())//go through all the polls
                 {
                     foreach (String hashtag in hashtags)//if the tweet contains the poll Id as a hashtag
                     {
-                        if (hashtag.ToLowerInvariant().Equals(poll.Id.ToLowerInvariant()))
+                        if (hashtag.ToLowerInvariant().Equals(poll.Id.ToLowerInvariant()) && poll.End_Time >= DateTime.Now)
                         {
                             String test = t.Tweet.Text.Replace("#" + poll.Id, "").ToLowerInvariant();
                             foreach (Result result in poll.Results.ToArray())//go through the results of that poll
